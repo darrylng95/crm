f551bc024f1b58833a551405144ee45d417c7343
PhuaYK <44082510+PhuaYK@users.noreply.github.com>
2019-12-20 12:28:37 +0800

    Update App.js

--- frontend/crm_app/src/app/App.js
+++ frontend/crm_app/src/app/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css';
 import ViewCustomers from '../containers/ViewCustomers';
+import MainPage from '../containers/MainPage';
 
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-         
-        </p>
-        <ViewCustomers />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </header>
+    <div className="wrapper">
+      <div className="content-wrapper">
+        <Switch>
+        <Route
+          path="/viewCustomers"
+          name="ViewCustomers"
+          component={ViewCustomers}
+        />
+        <Route
+          path="/mainPage"
+          name="mainPage"
+          component={MainPage}
+        />
+        <Route path="/" name="" component={MainPage}/>
+        </Switch>
+      </div>
     </div>
   );
 }
